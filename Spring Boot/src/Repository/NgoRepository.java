3b98b1431033a745fd958c9ce60d933c1a869469
AnandJoy7 <88574279+AnandJoy7@users.noreply.github.com>
2023-10-06 11:01:21 +0530

    Delete Spring Boot/src directory

--- Spring Boot/src/Repository/NgoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.syngrow.Repository;
-
-import com.springboot.syngrow.Model.Ngo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NgoRepository extends JpaRepository <Ngo,String>{
-}
